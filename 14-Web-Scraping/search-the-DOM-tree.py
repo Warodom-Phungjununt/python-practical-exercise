3f2dc9d77d468f4130dfd08a6409cff83a652211
Warodom-Phungjununt <155611313+Warodom-Phungjununt@users.noreply.github.com>
2024-03-31 15:20:35 +0000

    31 March 2024

--- 14-Web-Scraping/search-the-DOM-tree.py
+++ 14-Web-Scraping/search-the-DOM-tree.py
@@ -1,3 +1,5 @@
+# Introduction
+
 ### For mode information
 ### https://www.crummy.com/software/BeautifulSoup/bs4/doc/
 
@@ -21,7 +23,44 @@ example = '''
 
 ### Specify type of parser as htmal.parser
 s = BeautifulSoup(example, 'html.parser')
-#print(s)
+print(s)
 
 ### Using prettify() for arranging data from html file
 print(s.prettify())
+
+# Search the Tree
+### Find the Tag Name
+### The easiest way to extract element from the HTML is using Tag Name
+### For example, <h1> or <img> can be extracted by using .h1 or .img from BeautifulSoup Object
+print(s.h1)
+print(s.img)
+
+### For extracting the element within the element
+### .body.div can be used
+print(s.body.prettify())
+print('----------------')
+print(s.body.div.prettify())
+
+### Not only calling <h1> from .h1, but also calling find('h1')
+print(s.h1)
+print(s.find('h1'))
+
+# Find Multiple Elements
+###### If there are more than one tag within a DOM tree, find() function will find the latest result
+print(s.div)
+print(s.find('div'))
+###### Finding all element
+print(s('div'))
+print(s.find_all('div'))
+print(s('div')[1])
+
+# Filters
+###### Filter for element finding from message, attribute or class can be able to perform
+# ค้นหาจากข้อความ
+print(s.find(string='aHeader'))
+# ค้นหาจาก attribute (href) ของ element 
+print(s.find(href='#'))
+# ค้นหาจาก attribute (class) ของ element 
+print(s.find(class_='section1'))
+# ค้นหาจากหลาย attributes
+print(s.find(attrs={'class': 'section1'}))
